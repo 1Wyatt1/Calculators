edaf78bb84e778d95070aabf87d1bf25d1c62283
bioniclambda <80077386+bioniclambda@users.noreply.github.com>
2021-06-29 20:19:15 -0700

    add more operators

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,3 +1,22 @@
+print('Please separate all numbers and operators with a space')
+print('I.e. `8 + 2` not `8+2`')
+print('Possible operators: +, -, *, /, %, //')
+
 while True:
     expression = input('Expression: ')
-    print(eval(expression))
+
+    expression = expression.split(' ')
+
+    if expression[1] == '+':
+        print(str(float(expression[0]) + float(expression[2])))
+    elif expression[1] == '-':
+        print(str(float(expression[0]) - float(expression[2])))
+    elif expression[1] == '*':
+        print(str(float(expression[0]) * float(expression[2])))
+    elif expression[1] == '/':
+        print(str(float(expression[0]) / float(expression[2])))
+    elif expression[1] == '%':
+            print(str(float(expression[0]) % float(expression[2])))
+    elif expression[1] == '//':
+            print(str(float(expression[0]) // float(expression[2])))
+        
