f054aa923891fb3620ab3840abea6f8b00b34a34
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-07-07 14:35:00 +0100

    added modulo, typos, etc ..

--- C#/Program.cs
+++ C#/Program.cs
@@ -6,55 +6,32 @@ namespace Calculators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What math operation would you like to perform? (+,-,*,/");
+            Console.WriteLine("What math operation would you like to perform? (+,-,*,/,%)");
             string operation = Console.ReadLine();
             Console.WriteLine("Enter a number for X");
-            string num1 = Console.ReadLine();
+            float num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a number for Y");
-            string num2 = Console.ReadLine();
-            float num3 = Convert.ToInt32(num1);
-            float num4 = Convert.ToInt32(num2);
+            float num2 = Convert.ToInt32(Console.ReadLine());
             switch (operation)
             {
                 case "+":
-                    Addition(num3, num4);
+                    Console.WriteLine(num3 + num4);
                     break;
-
                 case "-":
-                    Subtraction(num3, num4);
+                    Console.WriteLine(num3 - num4);
                     break;
-
                 case "*":
-                    Multiplication(num3, num4);
+                    Console.WriteLine(num3 * num4);
                     break;
-
                 case "/":
-                    Division(num3, num4);
+                    Console.WriteLine(num3 / num4);
                     break;
-                
+                case "%":
+                    Console.WriteLine(num3 % num4);
                 default:
+                    Console.WriteLine("Unrecognized Operation {}", operation);
                     break;
             }
-        }
-
-        static void Addition(float num3, float num4)
-        {
-            Console.WriteLine(num3 + num4);
-        }
-
-        static void Subtraction(float num3, float num4)
-        {
-            Console.WriteLine(num3 - num4);
-        }
-
-        static void Multiplication(float num3, float num4)
-        {
-            Console.WriteLine(num3 * num4);
-        }
-
-        static void Division(float num3, float num4)
-        {
-            Console.WriteLine(num3 / num4);
         }
     }
 }
